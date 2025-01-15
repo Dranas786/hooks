c55304559fd1d30760879a468854d61482ad56ec
Dranas786 <dranas2002@gmail.com>
2025-01-15 16:37:49 -0700

    made react app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(4);
+
   return (
     <div>
       <button>-</button>
-      <span>0</span>
+      <span>{count}</span>
       <button>+</button>
     </div>
   );
